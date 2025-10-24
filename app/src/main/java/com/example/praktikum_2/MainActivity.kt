eb3f0793d1f10c60ef708f7b68989e53976412df
thekill022 <gibfathoni@gmail.com>
2025-10-24 14:59:00 +0700

    menambahkan text nama serta penyesuaian kembali UI

--- app/src/main/java/com/example/praktikum_2/MainActivity.kt
+++ app/src/main/java/com/example/praktikum_2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.praktikum_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.praktikum_2.ui.theme.Praktikum_2Theme
@@ -26,7 +28,7 @@ class MainActivity : ComponentActivity() {
             Praktikum_2Theme {
                 // A surface container using the 'background' color from the theme
                 Scaffold {
-                    innerPadding -> profile(modifier = Modifier.padding(innerPadding).padding(top = 20.dp).fillMaxSize())
+                    innerPadding -> profile(modifier = Modifier.padding(innerPadding).fillMaxSize().background(color = Color.Black))
                 }
             }
         }
--- app/src/main/java/com/example/praktikum_2/myprofile.kt
+++ app/src/main/java/com/example/praktikum_2/myprofile.kt
@@ -2,19 +2,26 @@ package com.example.praktikum_2
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -26,29 +33,46 @@ fun profile(modifier : Modifier) {
     ) {
         val image = painterResource(id = R.drawable.gibran)
 
-        Image(painter = image, contentDescription = "Gibran", modifier = Modifier
-            .size(size = 200.dp)
-            .clip(
-                CircleShape
-            )
-            .padding(bottom = 20.dp), contentScale = ContentScale.Crop)
+        Box(modifier = Modifier.padding(top = 20.dp)) {
+            Image(painter = image, contentDescription = "Gibran", modifier = Modifier
+                .size(size = 200.dp)
+                .clip(
+                    CircleShape
+                ), contentScale = ContentScale.Crop)
+        }
 
-        Row {
+        Row(modifier = Modifier.padding(top = 20.dp)) {
             val fb = painterResource(id = R.drawable.fb)
             val ig = painterResource(id = R.drawable.ig)
             val gmail = painterResource(id = R.drawable.gmail)
             val linkedin = painterResource(id = R.drawable.linkedin)
 
-            Image(painter = fb, contentDescription = "fb", modifier = Modifier.size(size = 50.dp).padding(end = 10.dp).clip(
-                CircleShape), contentScale = ContentScale.FillWidth)
-            Image(painter = ig, contentDescription = "ig", modifier = Modifier.size(size = 50.dp).padding(end = 10.dp).clip(
-                CircleShape), contentScale = ContentScale.FillWidth)
-            Image(painter = gmail, contentDescription = "gmail", modifier = Modifier.size(size = 50.dp).padding(end = 10.dp).clip(
-                CircleShape), contentScale = ContentScale.FillWidth)
-            Image(painter = linkedin, contentDescription = "linkedin", modifier = Modifier.size(size = 50.dp).padding(end = 10.dp).clip(
-                CircleShape), contentScale = ContentScale.Inside)
-
+            Image(painter = fb, contentDescription = "fb", modifier = Modifier
+                .size(size = 45.dp)
+                .clip(
+                    CircleShape
+                ), contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.padding(end = 10.dp))
+            Image(painter = ig, contentDescription = "ig", modifier = Modifier
+                .size(size = 45.dp)
+                .clip(
+                    CircleShape
+                ), contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.padding(end = 10.dp))
+            Image(painter = gmail, contentDescription = "gmail", modifier = Modifier
+                .size(size = 45.dp)
+                .clip(
+                    CircleShape
+                ), contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.padding(end = 10.dp))
+            Image(painter = linkedin, contentDescription = "linkedin", modifier = Modifier
+                .size(size = 45.dp)
+                .clip(
+                    CircleShape
+                ), contentScale = ContentScale.Crop)
         }
+        
+        Text(stringResource(id = R.string.name), modifier = Modifier.padding(top = 15.dp), fontSize = 40.sp, fontWeight = FontWeight.Bold, color = Color.White)
 
     }
 
