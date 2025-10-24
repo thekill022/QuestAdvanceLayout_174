70019c16a8758517d04718b5a0796aae95d5dd40
thekill022 <gibfathoni@gmail.com>
2025-10-24 09:36:37 +0700

    mebambahkan tata letak column untuk component setelah spacer

--- app/src/main/java/com/example/praktikum_2/content.kt
+++ app/src/main/java/com/example/praktikum_2/content.kt
@@ -14,8 +14,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +37,9 @@ fun layout(modifier : Modifier) {
                 .size(size = 100.dp)
                 .padding(5.dp))
             Spacer(modifier = Modifier.width(30.dp))
+            Column {
+
+            }
 
         }
     }
