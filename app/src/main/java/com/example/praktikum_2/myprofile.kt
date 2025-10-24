2721762383274ed364eddf12e259ffe98324ac89
thekill022 <gibfathoni@gmail.com>
2025-10-24 14:10:54 +0700

    membuat tata letak column untuk pembungkus component

--- app/src/main/java/com/example/praktikum_2/myprofile.kt
+++ app/src/main/java/com/example/praktikum_2/myprofile.kt
@@ -1,10 +1,19 @@
 package com.example.praktikum_2
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun profile(modifier : Modifier) {
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 
 }
