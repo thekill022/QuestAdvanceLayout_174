51560310cafa6e33cb79aefa077194eb27db803c
thekill022 <gibfathoni@gmail.com>
2025-10-24 15:39:15 +0700

    menambahkan copyright

--- app/src/main/java/com/example/praktikum_2/myprofile.kt
+++ app/src/main/java/com/example/praktikum_2/myprofile.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -117,11 +118,13 @@ fun profile(modifier : Modifier) {
                     Text(text = value, modifier = Modifier.weight(1f), fontWeight = FontWeight.Bold)
                     
                     Icon(imageVector = Icons.Sharp.PlayArrow, contentDescription = "arrow right", modifier = Modifier.size(22.dp))
-                    
                 }
             }
         }
 
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(stringResource(id = R.string.copy), color = Color.White, modifier = Modifier.align(alignment = Alignment.BottomCenter).padding(bottom = 10.dp))
+        }
 
     }
 
