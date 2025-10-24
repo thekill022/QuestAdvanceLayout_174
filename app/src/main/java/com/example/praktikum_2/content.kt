de16c1b2f09627016480cfb941ea86b28b710ab5
thekill022 <gibfathoni@gmail.com>
2025-10-24 09:15:45 +0700

    Membuat project dan membuat file content.kt

---
+++ app/src/main/java/com/example/praktikum_2/content.kt
@@ -0,0 +1,20 @@
+package com.example.praktikum_2
+
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun layout(modifier : Modifier) {
+}
