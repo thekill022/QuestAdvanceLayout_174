a5b71a5f76dd68573910a286b2ead6c3fed2c274
thekill022 <gibfathoni@gmail.com>
2025-10-24 09:23:45 +0700

    meload gambar dari resource drawable image

--- app/src/main/java/com/example/praktikum_2/content.kt
+++ app/src/main/java/com/example/praktikum_2/content.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -21,8 +22,10 @@ fun layout(modifier : Modifier) {
         Text(stringResource(id = R.string.title), fontSize = 20.sp, fontWeight = FontWeight.Bold)
         Text(stringResource(id = R.string.univ), modifier = Modifier.padding(bottom = 20.dp))
 
-        Row(modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp)) {
-
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp)) {
+            val image = painterResource(id = R.drawable.umy)
         }
     }
     }
