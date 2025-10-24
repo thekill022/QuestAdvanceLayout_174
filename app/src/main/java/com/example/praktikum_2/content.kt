e9fc8175ba543efc12c0cc51ca50f1ec457ee0ae
thekill022 <gibfathoni@gmail.com>
2025-10-24 09:33:34 +0700

    menambahkan spacer untuk jarak

--- app/src/main/java/com/example/praktikum_2/content.kt
+++ app/src/main/java/com/example/praktikum_2/content.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,7 +31,10 @@ fun layout(modifier : Modifier) {
             .padding(start = 20.dp, end = 20.dp)) {
             val image = painterResource(id = R.drawable.umy)
 
-            Image(painter = image, contentDescription = "logo umy", modifier = Modifier.size(size = 100.dp).padding(5.dp))
+            Image(painter = image, contentDescription = "logo umy", modifier = Modifier
+                .size(size = 100.dp)
+                .padding(5.dp))
+            Spacer(modifier = Modifier.width(30.dp))
 
         }
     }
